a00859e166ac5ef5c246818827805ede98beeb23
Maryam Kaveh <maryamk61@gmail.com>
2024-08-02 12:43:04 +0330

    Initial Commit

---
+++ WaveEffectModifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WaveEffectModifier
+//
+//  Created by Maryam Kaveh on 5/12/1403 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WaveEffectModifier/WaveEffectModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveEffectModifierApp.swift
+//  WaveEffectModifier
+//
+//  Created by Maryam Kaveh on 5/12/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct WaveEffectModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
